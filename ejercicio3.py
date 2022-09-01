e64b7410fd008cbbef6c83308ba31e3ab7519fa6
Fumito10 <98134809+Fumito10@users.noreply.github.com>
2022-09-01 00:10:04 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,19 +1,16 @@
-import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium .webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path=
     r"D:\Documents\chromedriver.exe");
 
+
 driver.get("https://www.clima.com/")
-time.sleep(3)
-driver.get("https://www.clima.com/mexico/")
-time.sleep(3)
-driver.get("https://www.clima.com/mexico/estado-de-queretaro-de-arteaga/")
-time.sleep(3)
-driver.get("https://www.clima.com/mexico/estado-de-queretaro-de-arteaga/queretaro/")
-time.sleep(3)
-driver.get("https://www.clima.com/mexico/estado-de-queretaro-de-arteaga/queretaro/por-horas")
-time.sleep(3)
+driver.set_window_size(790, 816)
+driver.find_element(By.CSS_SELECTOR, ".m_list_countrys_mx > a").click()
+driver.find_element(By.CSS_SELECTOR, ".navbar-i-search").click()
+driver.find_element(By.ID, "term").send_keys("Querét")
+driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) .item-name").click()
+driver.find_element(By.LINK_TEXT, "Por horas").click()
