73c954a92bd3789a35c1109d4153b10c4b1c8b33
Fumito10 <98134809+Fumito10@users.noreply.github.com>
2022-09-01 14:04:36 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -8,9 +8,17 @@ driver = webdriver.Chrome(executable_path=
 
 
 driver.get("https://www.clima.com/")
+time.sleep(2)
+driver.maximize_window()
+time.sleep(2)
 driver.set_window_size(790, 816)
+time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, ".m_list_countrys_mx > a").click()
-driver.find_element(By.CSS_SELECTOR, ".navbar-i-search").click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="page"]/header/div[2]/div[3]/ul/li/button').click()
+time.sleep(2)
 driver.find_element(By.ID, "term").send_keys("Querétaro")
+time.sleep(2)
 driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) .item-name").click()
+time.sleep(2)
 driver.find_element(By.LINK_TEXT, "Por horas").click()
